f2fa8f23125fe2987198ed02f2afb9d0e35d40e5
Meo_supper <nguyenvanthinh272454254@gmail.com>
2021-08-15 15:06:59 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.scss';
-import HomePage from './pages/HomePage'
+// import HomePage from './pages/HomePage'
+
+const reducer = (state, action) => {
+  switch (action) {
+    case 'Tang':
+      return state + 1;
+    case 'Giam':
+      return state - 1;
+    case 'XOA_TAT_CA':
+      return 0;
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, 0)
   return (
     <div>
-     <HomePage/>
+      <p>count: {count}</p>
+      <button onClick={()=> dispatch('Tang')}>Tang</button>
+      <button onClick={()=> dispatch('Giam')}>Giam</button>
+      <button onClick={()=> dispatch('XOA_TAT_CA')}> Xoa het du lieu</button>
+
     </div>
   );
 }
